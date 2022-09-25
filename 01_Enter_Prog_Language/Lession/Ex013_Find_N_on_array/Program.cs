1b4fab20302835740b0bb7a80d9dd95e8db91eb7
wall <wallenteen@gmail.com>
2022-09-25 13:29:39 +0700

    add hw10

--- 01_Enter_Prog_Language/Lession/Ex013_Find_N_on_array/Program.cs
+++ 01_Enter_Prog_Language/Lession/Ex013_Find_N_on_array/Program.cs
@@ -1,21 +1,30 @@
-﻿int count = 10;
+﻿//Console.Clear();
+int count = 10;
 int[] array = new int[10];
-int find = new Random().Next(1, 20);
+int find = new Random().Next(1, 10);
 int index = 0;
 Console.Write("Find ");
 Console.WriteLine(find);
 
 while (index < count)
 {
-    array[index] = new Random().Next(1, 100);
-    Console.Write(array[index]+" ");
+    array[index] = new Random().Next(1, 10);
+    Console.Write(array[index] + " ");
     index++;
 }
 index = 0;
+bool element = false;
 
 while (index < count)
 {
-    if (array[index] == find) Console.WriteLine("Yes");
-    index++;
+    if (array[index] == find)
+    {
+        element = true;
+        index++;
+    }
 }
-Console.WriteLine("end");
+
+if (element)
+{
+    Console.WriteLine("\nThis number have");
+}
--- 01_Enter_Prog_Language/Lession/Example012_random_and_max_N/Program.cs
+++ 01_Enter_Prog_Language/Lession/Example012_random_and_max_N/Program.cs
@@ -1,13 +1,11 @@
-﻿int n = new Random().Next(10, 100);//(10,1000]
+﻿int n = new Random().Next(10, 100);//(10,100]
 Console.Write("Привет, загадано число ");
 Console.WriteLine(n);
 
 int a = n / 10;
 int b = n % 10;
 Console.Write("Максимальное число ");
-if (a>b){
-    Console.WriteLine(a);
-}
-else{
-    Console.WriteLine(b);
-}
+if (a > b) Console.WriteLine(a);
+else Console.WriteLine(b);
+Console.WriteLine("n/10=" + a);
+Console.WriteLine("n%10=" + b);
